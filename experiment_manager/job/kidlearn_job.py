6c0f993276cc29a1767fccc943b3cfe96871ab15
bclement14 <benji.clement@gmail.com>
2015-11-04 18:27:20 +0100

    kidlearn_job update

--- experiment_manager/job/kidlearn_job.py
+++ experiment_manager/job/kidlearn_job.py
@@ -13,9 +13,11 @@ from .classic_job import IteratedJob
 class KidlearnJob(IteratedJob):
 
     def script(self):
+        xp = slef.gen_xp_to_optimize(self.data)
+
         cost = {}
-        nb_step = self.data.nb_step
-        for key,val in self.data.groups.items():
+        nb_step = xp.nb_step
+        for key,val in xp.groups.items():
             yolo = copy.deepcopy(val)
             yolo[0].run(nb_step)
             cost[key] = copy.deepcopy(yolo[0].calcul_cost())
@@ -25,17 +27,22 @@ class KidlearnJob(IteratedJob):
         with open("cost.json","w") as f:
             f.write(jstr)
 
-        self.data = None
+        
 
-    def gen_xp_to_optimize(zpdes_confs,xp_conf,ref_xp="optimize",nb_stud=1000,nb_step=100, base_path_to_save="experimentation/data/"):
-        stud = k_lib.student.KTstudent(params=xp_conf["stud"])
+    def gen_multi_zpdes(xp_conf,ref_xp="optimize",nb_stud=1000,nb_step=100, base_path_to_save="experimentation/data/"):
+        stud_confs = xp_conf["students"]
+        zpdes_confs = xp_conf["zpdes_conf"]
+        nb_stud = len(stud_confs)
+        nb_step = xp_conf["nb_steps"]
 
         wkgs = {}
         for ref,conf in zpdes_confs.items():
             zpdes = k_lib.seq_manager.ZpdesHssbg(params=conf)
+            
             wss = []
             for k in range(nb_stud):
-                wss.append(k_lib.experimentation.WorkingSession(student=copy.deepcopy(stud), seq_manager=copy.deepcopy(zpdes)))
+                stud = k_lib.student.KTstudent(params=stud_confs[k])
+                wss.append(k_lib.experimentation.WorkingSession(student=stud, seq_manager=copy.deepcopy(zpdes)))
             wkgs["zpdes_{}".format(ref)] = [k_lib.experimentation.WorkingGroup(WorkingSessions=wss)]
 
         xp = k_lib.experimentation.Experiment(WorkingGroups=wkgs,
