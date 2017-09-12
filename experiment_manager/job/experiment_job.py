7ec3c2e3b1e101728071916f69204be5595d8104
William Schueller <william.schueller@gmail.com>
2017-09-12 17:36:27 +0200

    added sqlite3 by default to experiment_jobs

--- experiment_manager/job/experiment_job.py
+++ experiment_manager/job/experiment_job.py
@@ -786,7 +786,7 @@ class MultipleGraphExpDBJobNoStorage(MultipleGraphExpDBJob):
 			self.db.reconnect()
 		self.data = {}
 		if self.dep_path and (not self.db.id_in_db(xp_uuid=self.xp_uuid) or int(self.db.get_param(param='Tmax', xp_uuid=self.xp_uuid))<self.graph_cfg['tmax']):
-			dep_db = self.db.__class__(db_type="sqlite3",path=os.path.join(self.get_back_path(),self.dep_path,'naminggames.db'),db_type="sqlite3")
+			dep_db = self.db.__class__(db_type="sqlite3",path=os.path.join(self.get_back_path(),self.dep_path,'naminggames.db'))
 			dep_db.export(other_db=self.db, id_list=[self.xp_uuid], methods=self.methods)
 			self.data['exp'] = self.db.get_experiment(xp_uuid=self.xp_uuid)
 			source_file = os.path.join(self.get_back_path(),self.dep_path,'data',self.xp_uuid+'.db.xz')
