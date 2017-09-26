8a8a1ea7371be70e7fb6697e50e737d43789b3c8
William Schueller <william.schueller@gmail.com>
2017-09-26 17:41:52 +0200

    template job

--- experiment_manager/__init__.py
+++ experiment_manager/__init__.py
@@ -1,7 +1,7 @@
 from ._version import __version__
 
-from .job import Job
-from .job_queue import JobQueue
-from .batchexp import BatchExp
+#from .job import Job
+#from .job_queue import JobQueue
+#from .batchexp import BatchExp
 
 import tools
