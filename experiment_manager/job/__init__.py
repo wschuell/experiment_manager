a7c905b83f5e0e81b81772b36a9a13d430c45925
William Schueller <william.schueller@gmail.com>
2017-09-26 17:39:30 +0200

    template job

--- experiment_manager/job/__init__.py
+++ experiment_manager/job/__init__.py
@@ -1,8 +1,8 @@
 from .job import Job
 from ._example_job import ExampleJob
-from .classic_job import ClassicJob, IteratedJob
-from .experiment_job import ExperimentJob, GraphExpJob, ExperimentDBJob, GraphExpDBJob, MultipleGraphExpDBJob
-from .cjob import CJob
+#from .classic_job import ClassicJob, IteratedJob
+#from .experiment_job import ExperimentJob, GraphExpJob, ExperimentDBJob, GraphExpDBJob, MultipleGraphExpDBJob
+#from .cjob import CJob
 
 import os
 import time
